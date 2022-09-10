0d85415f9195df318140d3f9325ec3f3a99e286d
olamideare <63550820+olamideare@users.noreply.github.com>
2022-09-10 11:10:01 +0100

    DRY

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -30,19 +30,18 @@ export default function Card(props) {
 
     // TO KEEP OUR CODE DRYER, WE CAN USE THE PROPS METHOD BELOW
 
-// return (
-//     <div className="card">
-//         {badgeText && <div className="card--badge">{badgeText}</div>}
-//         <img src={`../images/${props.item.coverImg}`} className="card--image" />
-//         <div className="card--stats">
-//             <img src="../images/star.png" className="card--star" />
-//             <span>{props.item.stats.rating}</span>
-//             <span className="gray">({props.item.stats.reviewCount}) • </span>
-//             <span className="gray">{props.item.location}</span>
-//         </div>
-//         <p className="card--title">{props.item.title}</p>
-//         <p className="card--price"><span className="bold">From ${props.item.price}</span> / person</p>
-//     </div>
-)
+    // return (
+    //     <div className="card">
+    //         {badgeText && <div className="card--badge">{badgeText}</div>}
+    //         <img src={`../images/${props.item.coverImg}`} className="card--image" />
+    //         <div className="card--stats">
+    //             <img src="../images/star.png" className="card--star" />
+    //             <span>{props.item.stats.rating}</span>
+    //             <span className="gray">({props.item.stats.reviewCount}) • </span>
+    //             <span className="gray">{props.item.location}</span>
+    //         </div>
+    //         <p className="card--title">{props.item.title}</p>
+    //         <p className="card--price"><span className="bold">From ${props.item.price}</span> / person</p>
+    //     </div>)
   );
 }
